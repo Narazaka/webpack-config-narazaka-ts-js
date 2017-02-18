9a80c46960f9ccbfd69785bdf1aaed2c4581d771
Narazaka <info@narazaka.net>
2017-02-18 18:06:36 +0900

    style

--- webpack.config.node.base.js
+++ webpack.config.node.base.js
@@ -6,26 +6,26 @@ module.exports = {
     rules: [
       {
         enforce: "pre",
-        test:   /\.js$/,
-        loader: ["eslint-loader"],
+        test:    /\.js$/,
+        loader:  ["eslint-loader"],
       },
       {
         enforce: "pre",
-        test:   /\.ts$/,
-        use: ["tslint-loader"],
+        test:    /\.ts$/,
+        use:     ["tslint-loader"],
       },
       {
         test:    /\.js$/,
-        use: ["babel-loader"],
+        use:     ["babel-loader"],
         exclude: /node_modules/,
       },
       {
-        test:    /\.ts$/,
-        use: [
+        test: /\.ts$/,
+        use:  [
           "babel-loader",
           {
-            loader: "ts-loader",
-            options:     {
+            loader:  "ts-loader",
+            options: {
               compilerOptions: {
                 rootDir:        "src",
                 outDir:         "dist",
